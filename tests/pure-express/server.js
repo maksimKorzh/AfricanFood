ebfe12dce945e9682bfc684d5e1f3591a9c1d77b
Maksim Korzh <freesoft.for.people@gmail.com>
2019-04-02 12:14:54 +0300

    mastering api interaction

--- tests/pure-express/server.js
+++ tests/pure-express/server.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
+const members = require('./members');
+const logger = require('./middleware/logger');
 
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
-  next();
-}
 
 // Init middleware
-app.use(logger);
+//app.use(logger);
 
 // Gets all members
 app.get('/api/members', (reg, res) => res.json(members));
+
+// Get single member
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
